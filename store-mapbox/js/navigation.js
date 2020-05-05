982ac7a7b0bca493a259e58da53801d341b0b180
Roque Leal <32916827+roqueleal@users.noreply.github.com>
2020-05-04 23:00:13 -0500

    ed
    de

---
+++ store-mapbox/js/navigation.js
@@ -0,0 +1,18 @@
+function flyToStore(currentFeature) {
+  map.flyTo({
+    center: currentFeature.geometry.coordinates,
+    zoom: 15
+  });
+}
+
+function createPopUp(currentFeature) {
+  var popUps = document.getElementsByClassName('mapboxgl-popup');
+  // Check if there is already a popup on the map and if so, remove it
+  if (popUps[0]) popUps[0].remove();
+
+  var popup = new mapboxgl.Popup({ closeOnClick: false })
+    .setLngLat(currentFeature.geometry.coordinates)
+    .setHTML('<h3>' + currentFeature.properties.Main4 + '</h3>' +
+    '<img src=" ' + currentFeature.properties.Photo + '" />')
+    .addTo(map);
+}
